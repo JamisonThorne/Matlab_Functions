7ec0332bc46eef279a8697c5f97a343ee9ace726
M237490 <thorne.jamison@mayo.edu>
2024-03-06 11:16:36 -0600

    Committing initial template.m used as a sample script for function documentation

---
+++ _Template.m
@@ -0,0 +1,18 @@
+function [] = function_name(input1)
+    %{
+    Example Setup:
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %     Start Example Code
+    input1 = 'blah'
+    [] = function_name(input1)
+    %     End Example Code
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function_name(inputs) -> (type)
+    
+    Paragraph Describing What Function Does
+    
+    >>> initialize_excel_doc('my_folder/results/','my_excel_document_name')
+    'my_folder/results/my_excel_document_name.xlsx'
+    %}
+    example(example);   %   Example
+end
